c40974e7cceb2f713a8ebefb7ef37fc10b0f7d75
phi <phi1618jp@gmail.com>
2015-11-18 18:58:20 +0900

    fix app

--- index.js
+++ index.js
@@ -7,115 +7,26 @@ var fs = require('fs');
 var app = require('app');
 // ウィンドウを作成するモジュール
 var BrowserWindow = require('browser-window');
-var dialog = require('dialog');
-var Menu = require('menu');
 // 起動 URL
-// var currentURL = 'file://' + __dirname + '/index.html';
-var currentURL = 'http://lite.runstant.com';
+var currentURL = 'file://' + __dirname + '/index.html';
 
 // クラッシュレポート
 require('crash-reporter').start();
-
-var openDialog = function() {
-  var win = BrowserWindow.getFocusedWindow();
-  dialog.showOpenDialog(
-    win,
-    {
-      properties: ['openFile'],
-    },
-    function(filename) {
-      fs.readFile(filename[0], function(error, text) {
-        console.log(text.toString());
-      });
-    });
-};
-
-// setup menu
-var menu = Menu.buildFromTemplate([
-  {
-    label: 'Runstant',
-    submenu: [
-      {
-        label: 'About',
-        click: function() {
-          mainWindow.loadUrl('http://lite.runstant.com/about');
-        }
-      },
-      { label: "Quit", accelerator: "Command+Q", click: function() { app.quit(); }},
-    ]
-  },
-  {
-    label: 'File',
-    submenu: [
-      { label: 'New File' },
-      {
-        label: 'Open',
-        click: function() {
-          openDialog();
-        },
-      },
-      { type: 'separator' },
-      {
-        label: "Save",
-        accelerator: "CmdOrCtrl+S",
-        click: function() {
-          console.log('save');
-        }
-      },
-      {
-        label: 'Save As...',
-      },
-    ]
-  },
-  {
-    label: 'Edit',
-    submenu: [
-      { label: "Copy", accelerator: "CmdOrCtrl+C", selector: "copy:" },
-      { label: "Paste", accelerator: "CmdOrCtrl+V", selector: "paste:" },
-      // {label: 'Copy', accelerator: 'Command+C', selector: 'copy'},
-      // {label: 'Paste', accelerator: 'Command+V', selector: 'paste'}
-    ]
-  },
-  {
-    label: 'View',
-    submenu: [
-      {
-        label: 'Reload',
-        accelerator: 'CmdOrCtrl+R',
-        click: function() { mainWindow.restart(); }
-      },
-      {
-        label: 'Toggle Full Screen',
-        accelerator: 'CmdOrCtrl+Ctrl+F',
-        click: function() {
-          mainWindow.setFullScreen(!mainWindow.isFullScreen());
-        }
-      },
-      {
-        label: 'Toggle Developer Tools',
-        accelerator: 'CmdOrCtrl+Alt+I',
-        click: function() {
-          mainWindow.toggleDevTools();
-        }
-      },
-    ],
-  }
-]);
 
 // メインウィンドウはGCされないようにグローバル宣言
 var mainWindow = null;
 
 // 全てのウィンドウが閉じたら終了
 app.on('window-all-closed', function() {
-  if (process.platform != 'darwin') {
+  // if (process.platform != 'darwin') {
     app.quit();
-  }
+  // }
 });
 
 // Electronの初期化完了後に実行
 app.on('ready', function() {
   // メイン画面の表示。ウィンドウの幅、高さを指定できる
-  mainWindow = new BrowserWindow({width: 800, height: 600, 'node-integration': false, });
+  mainWindow = new BrowserWindow({width: 800, height: 600 });
 
   // and load the index.html of the app.
   mainWindow.loadUrl(currentURL);
@@ -128,7 +39,5 @@ app.on('ready', function() {
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
-
-  Menu.setApplicationMenu(menu);
 });
 
